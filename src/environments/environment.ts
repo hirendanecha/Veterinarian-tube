53421bc47ab0867e9999b4d7e95968bfa4019a82
krp-opash <krp.opash@gmail.com>
2024-03-20 12:49:01 +0530

    add captcha and replace text

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,5 +11,7 @@ export const environment = {
   socketUrl: `${url}/`,
   webUrl: webUrl,
   tubeUrl: tubeUrl,
-  domain: '.veterinarian.tube'
+  domain: '.veterinarian.tube',
+  siteKey: '0x4AAAAAAAUwBojCBa1YQfJt',
+  secretKey: '0x4AAAAAAAUwBhIek0zdK4eShLRfbRAOAUQ',
 };
